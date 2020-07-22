1fc3986251a3600a22820c987c565613d9ad9060
TheJang <wkddnjs7310@naver.com>
2020-07-22 17:27:40 +0900

    init laundary_web

---
+++ laundary_web/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Test from "./Header";
+import { Grid } from "@material-ui/core"
+
+const App = () => {
+
+  return (
+    <Grid>
+      <Test/>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ laundary_web/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <div>
+            여기는 탑 
+        </div>
+    )
+}
+
+export default Header;
+
